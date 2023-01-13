8d987b320b321c4d95bdb3a2110b5f295f82894c
Muhammed Rashid <rashidktr07@gmail.com>
2023-01-13 21:43:59 +0530

    Create about page design

--- src/Components/AdobeWebsitePage/Home.js
+++ src/Components/AdobeWebsitePage/Home.js
@@ -4,11 +4,11 @@ import Logo from '../../Assets/photoshoplogo.svg'
 
 function Home() {
   return (
-    <div className='w-full min-h-[100vh] md:min-h-[60vh] lg:min-h-[80vh] bg-[rgb(245,245,245)] lg:relative lg:bg-transparent lg:flex lg:justify-start lg:items-center'>
+    <div className='w-full min-h-[60vh] lg:min-h-[80vh] bg-[rgb(245,245,245)] lg:relative lg:bg-transparent lg:flex lg:justify-start lg:items-center'>
       <div className="img-sec h-[270px] w-full bg-black lg:h-[87vh] lg:absolute lg:top-0 lg:left-0 -z-10">
         <img src={HomeBG} alt=""  className='h-full object-cover md:h-[270px] md:w-full lg:h-full'/>
       </div>
-      <div className="content p-5 md:px-16 lg:h-full lg:container lg:mx-auto lg:flex lg:flex-col">
+      <div className="content p-5 md:px-16 lg:h-full lg:container lg:mx-auto lg:flex lg:flex-col xl:px-32">
         <div className="photoshop-logo hidden lg:block lg:w-[4.5%] lg:order-1">
           <img src={Logo} alt="" />
         </div>
