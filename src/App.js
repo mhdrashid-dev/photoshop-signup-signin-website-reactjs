85f6f302b2740fd6b88c3b1bba8a196f70191e54
Muhammed Rashid <rashidktr07@gmail.com>
2023-01-13 12:59:36 +0530

    Adding Pages and Components folders

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AccountPage from './Pages/AccountPage'
+import AdobeWebsitePage from './Pages/AdobeWebsitePage'
+
+function App() {
+  return (
+    <div>
+      <AdobeWebsitePage></AdobeWebsitePage>
+      <AccountPage></AccountPage>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/AdobeWebsitePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdobeWebsitePage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AdobeWebsitePage
