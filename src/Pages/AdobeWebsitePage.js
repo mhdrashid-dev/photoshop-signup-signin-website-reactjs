0a3309be23679108155b03c2777f7d6298b13fe9
Muhammed Rashid <rashidktr07@gmail.com>
2023-01-13 13:02:23 +0530

    Starting Design Header page

--- src/Pages/AdobeWebsitePage.js
+++ src/Pages/AdobeWebsitePage.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import About from '../Components/AdobeWebsitePage/About'
+import Article from '../Components/AdobeWebsitePage/Article'
+import Footer from '../Components/AdobeWebsitePage/Footer'
+import Header from '../Components/AdobeWebsitePage/Header'
+import Home from '../Components/AdobeWebsitePage/Home'
 
 function AdobeWebsitePage() {
   return (
     <div>
-      
+      <Header></Header>
+      <Home></Home>
+      <About></About>
+      <Article></Article>
+      <Footer></Footer>
     </div>
   )
 }
